f988e228533cc19fdbf8ab760e0fe25181e854f8
uado1104 <18819446829@163.com>
2024-10-28 00:01:10 +0800

    支持game

---
+++ client/assets/module_td/scripts/game.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('game')
+export class game extends Component {
+    start() {
+        console.log('game start');
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
