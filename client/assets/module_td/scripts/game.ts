14066838c7dc734161599ed7854d87ffdfb39a6d
uado <18819446829@163.com>
2024-11-09 22:36:17 +0800

    加入脚本

--- client/assets/module_td/scripts/game.ts
+++ client/assets/module_td/scripts/game.ts
@@ -1,16 +1,11 @@
 import { _decorator, Component, Node } from 'cc';
-import { ECSWorld } from './core/ecs/ECSWorld';
 const { ccclass, property } = _decorator;
 
 @ccclass('game')
 export class game extends Component {
-  private ECSWorld: ECSWorld = new ECSWorld(100);
-
   start() {
     console.log('game start');
   }
 
-  update(deltaTime: number) {
-    ECSWorld.Tick(deltaTime);
-  }
+  update(deltaTime: number) {}
 }
